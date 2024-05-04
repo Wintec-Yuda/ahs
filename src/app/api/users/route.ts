300706a2dc2ed67a3cb3176ac50512e1fc1596cb
Wintec-Yuda <alamasta004@gmail.com>
2024-05-04 12:26:02 +0700

    setup firebase

---
+++ src/app/api/users/route.ts
@@ -0,0 +1,23 @@
+import { getData } from "@/app/lib/firebase/service";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  try {
+    const data = await getData("users");
+    return NextResponse.json(
+      {
+        success: true,
+        data,
+      },
+      { status: 200 }
+    );
+  } catch (error) {
+    return NextResponse.json(
+      {
+        success: false,
+        message: "Internal Server Error",
+      },
+      { status: 500 }
+    );
+  }
+}
